42e54d324af2793390883fe7eca72d4da7f3ff61
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2018-06-17 11:03:54 +0200

    Added the Function to set a New statement

--- Ang.js
+++ Ang.js
@@ -1,12 +1,23 @@
 var app = angular.module("app",[]);
-app.controller("ctrl", function($scope) {
+app.controller("ctrl", function($scope,$http) {
+    $scope.Error=0
     $scope.SaveTime=function(){
         console.log($scope.From)
         console.log($scope.To)
         console.log($scope.Statement_encoded)
     }
+
     $scope.SaveStatement=function(){
-        console.log($scope.Statement_encoded_input)
-        console.log($scope.Statement_real)
+        if(typeof $scope.Statemnet_real=="undefined" || typeof $scope.Statement_encoded_input=="undefined"){
+            $scope.ShowNewStatementMenu=0;
+            $scope.Error="You Can't Leave Values Empty!";
+            return;
+        }
+        $http.get("/newstatement/"+$scope.Statemnet_real+"/"+$scope.Statement_encoded_input).then(
+        function(response) {
+          if (response.data=="ERROR"){
+                $scope.Error="Unable to Save New Statement Check the Service Logs";
+        }});
     }
+
 });
