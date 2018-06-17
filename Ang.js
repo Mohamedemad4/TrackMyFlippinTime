b9de4c9807298a2ebfb08ac9f1c746b152e2a668
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2018-06-17 11:55:03 +0200

    Added Function Savetime()

--- Ang.js
+++ Ang.js
@@ -1,10 +1,38 @@
 var app = angular.module("app",[]);
 app.controller("ctrl", function($scope,$http) {
+
     $scope.Error=0
     $scope.SaveTime=function(){
-        console.log($scope.From)
-        console.log($scope.To)
-        console.log($scope.Statement_encoded)
+        if(typeof $scope.From=="undefined" || typeof $scope.To=="undefined" || 
+            typeof $scope.Statement_encoded =="undefined"){
+            $scope.ShowNewStatementMenu=0;
+            $scope.Error="You Can't Leave Values Empty!";
+            return;
+        }
+
+        if ($scope.To.split(':')[0] > 23 || $scope.From.split(':')[0] > 23){
+            $scope.Error="You Can't have Hours after 23 (hint: try 0 instead of 24)"
+            return;
+        }
+        if ($scope.To.split(':')[1] > 60 || $scope.From.split(':')[1] > 60){
+            $scope.Error="You Can't have Minutes after 60"
+            return;
+        }
+        datefrom=new Date();
+        datefrom.setHours($scope.From.split(':')[0]);
+        datefrom.setMinutes($scope.From.split(':')[1]);
+
+        dateto=new Date();
+        dateto.setHours($scope.To.split(':')[0]);
+        dateto.setMinutes($scope.To.split(':')[1]);
+
+        dateFromUnix=datefrom.getTime()/1000
+        dateToUnix=dateto.getTime()/1000
+        $http.get("/deposit/"+dateFromUnix+"/"+dateToUnix+"/"+$scope.Statement_encoded).then(
+        function(response) {
+          if (response.data=="ERROR"){
+                $scope.Error="Unable to Save Time Check the Service Logs";
+        }});
     }
 
     $scope.SaveStatement=function(){
