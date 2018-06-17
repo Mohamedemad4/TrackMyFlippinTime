59c95bbeb832dba6eff55ce532a7b3dbd940b324
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2018-06-17 10:35:10 +0200

    Added Basic Angular.Js  stuff and the 2 functions from index.html

--- Ang.js
+++ Ang.js
@@ -1,11 +1,12 @@
 var app = angular.module("app",[]);
 app.controller("ctrl", function($scope) {
-    $scope.h1 ='Joke`s on you Hater';
-    $scope.fg=0;
     $scope.SaveTime=function(){
         console.log($scope.From)
+        console.log($scope.To)
+        console.log($scope.Statement_encoded)
     }
     $scope.SaveStatement=function(){
         console.log($scope.Statement_encoded_input)
+        console.log($scope.Statement_real)
     }
 });
