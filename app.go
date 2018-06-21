04dd4f6368a4e1fa7a94aab81f93bc21fa57a0b3
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2018-06-21 23:56:08 +0200

    Added /initPie/ for JS

--- app.go
+++ app.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "net/http"
+    "os"
     "log"
     "fmt"
-    "os"
-    "database/sql"
+    "time"
+    "net/http"
     "io/ioutil"
+    "database/sql"
     "encoding/json"
-    _ "github.com/mattn/go-sqlite3"
     "github.com/gorilla/mux"
+    _ "github.com/mattn/go-sqlite3"
 )
 
 var db, err = sql.Open("sqlite3", "./tmft_service.db")
@@ -19,14 +20,16 @@ func init(){
         log.Fatal(err)
     }
     if _, err := os.Stat("Ang.js"); os.IsNotExist(err){
-        response, err:= http.Get("https://raw.githubusercontent.com/Mohamedemad4/TrackMyFlippinTime/master/Ang.js")
+        response, err:= http.Get(
+            "https://raw.githubusercontent.com/Mohamedemad4/TrackMyFlippinTime/master/Ang.js")
         if err!=nil{log.Fatal(err)}
         body, err := ioutil.ReadAll(response.Body)
         if err!=nil{log.Fatal(err)}
         ioutil.WriteFile("Ang.js", body, 0644)
     }
     if _, err := os.Stat("index.html"); os.IsNotExist(err){
-        response, err:= http.Get("https://raw.githubusercontent.com/Mohamedemad4/TrackMyFlippinTime/master/index.html")
+        response, err:= http.Get(
+            "https://raw.githubusercontent.com/Mohamedemad4/TrackMyFlippinTime/master/index.html")
         if err!=nil{log.Fatal(err)}
         body, err := ioutil.ReadAll(response.Body)
         if err!=nil{log.Fatal(err)}
@@ -48,6 +51,7 @@ func main() {
     router := mux.NewRouter()
     router.HandleFunc("/", rootPage).Methods("GET")
     router.HandleFunc("/Ang.js", JsPage).Methods("GET")
+    router.HandleFunc("/initPie/",initPie).Methods("GET")
     router.HandleFunc("/withdraw/{startstamp}/{endstamp}",withdraw).Methods("GET")
     router.HandleFunc("/deposit/{from}/{to}/{statement_encoded}",deposit).Methods("GET")
     router.HandleFunc("/newstatement/{statement}/{statement_encoded}",newstatement).Methods("GET")
@@ -156,4 +160,42 @@ func newstatement(w http.ResponseWriter,r *http.Request){
     }else{
         insertStat.Exec(statement, statement_encoded)
     }
+}
+
+func initPie(w http.ResponseWriter,r *http.Request){
+    var valtots float64
+    histroyDurations:=make(map[string]float64)
+    rDictHistory:=make(map[string]float64)
+
+    rows, err := db.Query("SELECT fromstamp,tostamp,statement_encoded from history")
+
+    if err != nil {
+        log.Println(err)
+    }else{
+       defer rows.Close()
+       for rows.Next() {
+            var fromstamp int
+            var tostamp int
+            var statement_encoded string
+            err = rows.Scan(&fromstamp,&tostamp,&statement_encoded)
+            if err != nil {log.Println(err)}
+            
+            hms:=time.Unix(int64(fromstamp),0).Sub(time.Unix(int64(tostamp),0)).Hours()*60
+            mms:=time.Unix(int64(fromstamp),0).Sub(time.Unix(int64(tostamp),0)).Minutes()
+            tots:=mms+hms
+            if val,ex:=histroyDurations[statement_encoded];ex{
+                histroyDurations[statement_encoded]=tots+val
+            }else{
+                histroyDurations[statement_encoded]=tots
+            }
+       }
+       err = rows.Err()
+       if err != nil {log.Println(err)}
+    }
+    for _,val:=range histroyDurations{valtots+=val}
+    
+    for key,val:=range histroyDurations{
+        rDictHistory[key]=val/valtots
+    }
+    json.NewEncoder(w).Encode(rDictHistory)  
 }
